e21a7d6b98a39d6643421ae383b32e0d54f0a0b1
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-08-14 14:56:38 -0400

    Glab309.5.3 Done

--- src/main/java/com/perscholas/TestController/UserController.java
+++ src/main/java/com/perscholas/TestController/UserController.java
@@ -1,12 +1,15 @@
 package com.perscholas.TestController;
 
 import com.perscholas.model.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
+
     @PostMapping("/users")
     public void  printData(@RequestBody User user){
         System.out.println("Printing the user data:" + user);
